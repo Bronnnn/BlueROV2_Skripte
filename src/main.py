eba8c6524825afdfdc19db1542817b656cb8e436
Bronnnn <scherrerm@web.de>
2023-03-23 23:03:08 +0100

    playing around with git

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import helpers
+from lol import hellow
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     helpers.hello_world()
+    hellow.hello()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
