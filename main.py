5b101d40acb8f0ae6e86334080bb3baa40b30a04
Bronnnn <scherrerm@web.de>
2023-03-24 04:10:03 +0100

    playing around with git

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src import helpers
 from src import Teststand_Motorsequenz
+from src import BlueSim_Motorsequenz
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # helpers.hello_world()
-    Teststand_Motorsequenz.run()
+    # Teststand_Motorsequenz.run()
+    BlueSim_Motorsequenz.run(conn_type="SC2A")
