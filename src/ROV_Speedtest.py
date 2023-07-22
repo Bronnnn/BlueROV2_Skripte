67ede87096cca8244ab45f024106f5c904d0b8ac
Julian Rico <julian.rico@hotmail.de>
2023-07-22 14:22:32 +0200

    Versuch MAV_CMD_DO_CHANGE_SPEED mit 1m/s

--- src/ROV_Speedtest.py
+++ src/ROV_Speedtest.py
@@ -22,7 +22,6 @@ def run(conn_type:str="SC2A"):
     Here: 3 * 2s per direction and 4 different directions = 24s (plus sleep time)
     """
     print("ROV Speedtest")
-    print("Send CMD 43000 to control speed ")
     print("'Ctrl + C' to disarm the BlueROV and exit the script")
     time.sleep(2)
 
@@ -52,7 +51,7 @@ def run(conn_type:str="SC2A"):
     helpers.disarm(master)
 
     # available modes: ['STABILIZE', 'ACRO', 'ALT_HOLD', 'AUTO', 'GUIDED', 'CIRCLE', 'SURFACE', 'POSHOLD', 'MANUAL']
-    flightmode = 'GUIDED'
+    flightmode = 'AUTO'
     print(f"Set flightmode to {flightmode}")
     helpers.change_flightmode(master, mode=flightmode)
 
@@ -67,21 +66,16 @@ def run(conn_type:str="SC2A"):
 
     ### BEGIN USER CODE ###
     
-    # (1) Set servo 1 to 1400 us
-     print("\nSending MAV_CMD_DO_SET_SERVO (178) to servo 1 with 1400us")
-    helpers.send_cmd_set_servo(master, 1, 1400)
+    test_set_servo_speed()
     
-    # (2) Wait 5s
-    print("\nWait 5 seconds...")
-    time_start = default_timer()
-    countdown = 5
-    while (default_timer() - time_start < countdown):
-        print(round(countdown - (default_timer() - time_start)))
-        time.sleep(1)    
-       
+    ### END USER CODE ###
     
-    """ MAV_CMD_GUIDED_CHANGE_SPEED
+    # clean up (disarm)
+    print("Program finished.")
+    helpers.disarm(master)
     
+    
+def test_guided_speed():
     # (1) Set speed to 1 m/s
     print("\nSending MAV_CMD_GUIDED_CHANGE_SPEED (43000) with 1 m/s")
     helpers.send_cmd_guided_change_speed(master, 1)
@@ -97,11 +91,29 @@ def run(conn_type:str="SC2A"):
     # (3) Set speed back to 0 m/s
     print("\nSending MAV_CMD_GUIDED_CHANGE_SPEED (43000) with 0 m/s")
     helpers.send_cmd_guided_change_speed(master, 0)
-    """
     
-    ### END USER CODE ###
+def test_set_servo_speed():
+        # (1) Set servo 1 to 1400 us
+    print("\nSending MAV_CMD_DO_SET_SERVO (183) to servo 1 with 1400us")
+    helpers.send_cmd_set_servo(master, 1, 1400)
     
-
-    # clean up (disarm)
-    print("Program finished.")
-    helpers.disarm(master)
+    # (2) Wait 5s
+    print("\nWait 5 seconds...")
+    time_start = default_timer()
+    countdown = 5
+    while (default_timer() - time_start < countdown):
+        print(round(countdown - (default_timer() - time_start)))
+        time.sleep(1)    
+        
+def test_set_speed():
+    # (1) Set speed to 1 m/s
+    print("\nSending MAV_CMD_DO_CHANGE_SPEED (178) with 1 m/s")
+    helpers.send_cmd_guided_change_speed(master, 1)
+    
+    # (2) Wait 5s
+    print("\nWait 5 seconds...")
+    time_start = default_timer()
+    countdown = 5
+    while (default_timer() - time_start < countdown):
+        print(round(countdown - (default_timer() - time_start)))
+        time.sleep(1)    
