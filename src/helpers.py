5273a8607d9b8e3f8f89cceb3b64abcf8e079b54
Bronnnn <scherrerm@web.de>
2023-09-02 22:08:43 +0200

    Binded a function to 'Ctrl + C' to disarm the bluerov, before stopping the exection of the script.

--- src/helpers.py
+++ src/helpers.py
@@ -255,14 +255,10 @@ def check_capabilities(master, verbose=3):
     if verbose>1: print(f"Autopilot version: {autopilot_version}")
     # check if autopilot supports commanding position and velocity targets in global scaled integers
     # Source: https://mavlink.io/en/messages/common.html#MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT
-    MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT = autopilot_version['capabilities'] & 256
-    print(MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT)
-    print(MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT==256)
-    print(autopilot_version['capabilities'])
-    print(autopilot_version['capabilities']==256)
-    if verbose>0: print(f"MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT: {MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT == 256}")
-    MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_LOCAL_NED = autopilot_version['capabilities'] & 128
-    if verbose>0: print(f"MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_LOCAL_NED: {MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_LOCAL_NED == 128}")
+    MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT = (autopilot_version['capabilities'] & 256) == 256
+    if verbose>0: print(f"MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT: {MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_GLOBAL_INT}")
+    MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_LOCAL_NED = (autopilot_version['capabilities'] & 128) == 128
+    if verbose>0: print(f"MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_LOCAL_NED: {MAV_PROTOCOL_CAPABILITY_SET_POSITION_TARGET_LOCAL_NED}")
 
 def init(master):
     # clean up (disarm)
